a60c52b95001472b1e66079cb58c585943dfb48a
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-05-22 20:19:10 -0500

    no tears left to cry

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # hogwarts sorting hat
+
+## Description
+When it is finished, this code will take a name, randomly assign it to a Hogwarts house, and print a card with the name and crest to the Dom.
+
+### Feature List
+* Show all Hogwarts Students
+* Add Students to the school
+* Random sorter
+
+## Screenshots
+Pending
+
+## How To Run
+1. Clone down this repo
+1. Use your favorite http server (like [http-server](https://www.npmjs.com/package/http-server)) to serve it up (`hs`)
+1. In your browser, go where it's being served (default is localhost:8080)
+
+## Contributors
+* [Gwynne Meeks](https://github.com/gwynnemeeks)
+
+## TODO/Feature Request
+- [ ] Fix Functions
+- [ ] Random number generator
+- [ ] Actually sort
+- [ ] Crying will stop
+
+code block example: (Pending)
+```js
+const addStudent = (nameArgument) => {
+    let newStudent = {
+        name: nameArgument,
+        house: 'Slytherin',
+        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest/scale-to-width-down/160?cb=20160729150848',
+        expelled: false,
+    }
+```
--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 let students = [
     {
         name: 'Hermione Granger',
-        house: 'Gryffindor',
-        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest/scale-to-width-down/182?cb=20160729145529',
+        house: 'https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest/scale-to-width-down/182?cb=20160729145529',
         expelled: false,
     },
     {
         name: 'Luna Lovegood',
-        house: 'Ravenclaw',
-        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945',
+        house: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945',
         expelled: false,
     },
     {
         name: 'Pansy Parkinson',
-        house: 'Slytherin',
-        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest/scale-to-width-down/160?cb=20160729150848',
+        house: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest/scale-to-width-down/160?cb=20160729150848',
         expelled: true,
     },
     {
         name: 'Hannah Abbott',
-        house: 'Hufflepuff',
-        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653',
+        house: 'https://vignette.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653',
         expelled: false,
     },
 ];
@@ -29,6 +25,7 @@ const printToDom = (selector, textToPrint) => {
     document.querySelector(selector).innerHTML = textToPrint;
   };
 
+  //function to build student house sorting cards
 const buildHouses = (studentBody) => {
     let domString = '';
 
@@ -39,8 +36,8 @@ const buildHouses = (studentBody) => {
                     <div class="card" style="width: 18rem;">
                         <div class="card students">
                             <h2 class="card-title">${studentBody[i].name}</h2>
-                            <img src="${studentBody[i].imgUrl}" class="card-img-top" alt="${studentBody[i].house}>
-                            <h4 class="card-title">${studentBody[i].house}</h4>
+                            <img src="${studentBody[i].house}" class="card-img-top">
+                            <br>
                         </div>
                     </div> 
                 </div>
@@ -48,22 +45,50 @@ const buildHouses = (studentBody) => {
         `;
     }
         printToDom('#dumbledoresArmy', domString);
-  }
+  };
 
-const addStudent = (nameArgument) => {
+// sorting hat random generator
+const sortingHat = () => {
+    newHouse = Math.floor(Math.random() * 4); {
+
+ // somehow: let students.house = newHouse
+
+    switch (newHouse) {
+        case 'Gryffindor': {
+            let newHouse = 'https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest/scale-to-width-down/182?cb=20160729145529';
+            break;
+        }
+        case 'Ravenclaw': {
+            let newHouse = 'https://vignette.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945';
+            break;
+        }
+        case 'Slytherin': {
+            let newHouse = 'https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest/scale-to-width-down/160?cb=20160729150848';
+            break;
+        }
+        case 'Hufflepuff': {
+            let newHouse = 'https://vignette.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653';
+            break;
+        }
+    }   
+}
+};
+
+// probably broke my spiffy function with this, but I tried anyway
+const addStudent = (nameArgument, newHouse) => {
     let newStudent = {
         name: nameArgument,
-        house: 'Slytherin',
-        imgUrl: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest/scale-to-width-down/160?cb=20160729150848',
+        house: newHouse,
         expelled: false,
     }
 
     students.push(newStudent);
     buildHouses(students);
+    //sortingHat(newHouse);
     console.log(students);
 };
-//testing addStudnet
-    addStudent('Gwynne Meeks');
+
+
 
 const clickEvents = () => {
     //document.querySelector()
